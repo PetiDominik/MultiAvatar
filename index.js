1620b92636f988a67f381c8321a42bdc09781518
PetiDominik <petid@wildom.com>
2023-11-24 09:33:39 +0100

    avatas: 400->1000

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 $(() => {
     const PARENT = $("article");
 
-    for (let index = 0; index < 401; index++) {
-        let code = `<div><img src="avatars/${index}.svg" /></div>`
+    for (let index = 0; index <= 1000; index++) {
+        let code = `<div><img src="avatars_bg/${index}.svg" /></div>`
         PARENT.append(code);
     }
 }) 
